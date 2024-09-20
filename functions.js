e78cac236a45096fcd1129670e92f0154369700d
Gania <ganiavin.gv@gmail.com>
2024-09-20 13:48:29 -0300

    FEAT: valor dinamico de input

---
+++ functions.js
@@ -0,0 +1,25 @@
+const b = document.getElementById("b")
+let res = document.querySelector("#res")
+
+function saberNumero(n){
+    const input = document.getElementById(n)
+    console.log(input.value)
+}
+
+function suma(a,b){
+    return a + b
+}
+
+function resta(a,b){
+    return a - b
+}
+
+function multiplicacion(a,b){
+    return a * b
+}
+
+function division(a,b){
+    return a / b
+}
+
+let = res()
